d15c6cd8909548b42ae75c5b35ba557fb2038087
Charlie <kiwichang2017@gmail.com>
2022-07-23 07:08:17 +0800

    Update IP

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function join(){
-    location.href = 'minecraft://?addExternalServer=牛舍伺服器|node2.ultimatesrv.com:25716'
+    location.href = 'minecraft://?addExternalServer=牛舍伺服器|asia7.asiannodes.host:25706'
 }
 window.onload = function changePic(){  
   const title = document.querySelector('#title')
